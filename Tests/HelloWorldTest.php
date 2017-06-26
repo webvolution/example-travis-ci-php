f7ad017cada2a6bdaa15fc9d3ba5eb776d098a95
sarn1 <sarn@tyndale.com>
2017-06-26 14:34:23 -0500

    fail

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -23,7 +23,7 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     {
         $helloWorld = new HelloWorld($this->pdo);
 
-        $this->assertEquals('Hello World', $helloWorld->hello());
+        $this->assertEquals('Good Bye World', $helloWorld->hello());
     }
 
     public function testHello()
